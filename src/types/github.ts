900829a7e34e065cc10748a7027a955274aec209
iddk <ldw940321@gmail.com>
2025-03-20 13:57:13 +0900

    feat: Repository 타입 수정

--- src/types/github.ts
+++ src/types/github.ts
@@ -1,59 +1,60 @@
 export type Repository = {
   id: number
-  node_id: string
   name: string
-  full_name: string
-  owner: Owner
-  private: boolean
-  html_url: string
   description: string | null
-  fork: boolean
-  url: string
-  archive_url: string
-  assignees_url: string
-  blobs_url: string
-  branches_url: string
-  collaborators_url: string
-  comments_url: string
-  commits_url: string
-  compare_url: string
-  contents_url: string
-  contributors_url: string
-  deployments_url: string
-  downloads_url: string
-  events_url: string
-  forks_url: string
-  git_commits_url: string
-  git_refs_url: string
-  git_tags_url: string
+  stargazers_count: number
+  updated_at: string | null
+  node_id?: string
+  full_name?: string
+  owner?: Owner
+  private?: boolean
+  html_url?: string
+  fork?: boolean
+  url?: string
+  archive_url?: string
+  assignees_url?: string
+  blobs_url?: string
+  branches_url?: string
+  collaborators_url?: string
+  comments_url?: string
+  commits_url?: string
+  compare_url?: string
+  contents_url?: string
+  contributors_url?: string
+  deployments_url?: string
+  downloads_url?: string
+  events_url?: string
+  forks_url?: string
+  git_commits_url?: string
+  git_refs_url?: string
+  git_tags_url?: string
   git_url?: string
-  issue_comment_url: string
-  issue_events_url: string
-  issues_url: string
-  keys_url: string
-  labels_url: string
-  languages_url: string
-  merges_url: string
-  milestones_url: string
-  notifications_url: string
-  pulls_url: string
-  releases_url: string
+  issue_comment_url?: string
+  issue_events_url?: string
+  issues_url?: string
+  keys_url?: string
+  labels_url?: string
+  languages_url?: string
+  merges_url?: string
+  milestones_url?: string
+  notifications_url?: string
+  pulls_url?: string
+  releases_url?: string
   ssh_url?: string
-  stargazers_url: string
-  statuses_url: string
-  subscribers_url: string
-  subscription_url: string
-  tags_url: string
-  teams_url: string
-  trees_url: string
+  stargazers_url?: string
+  statuses_url?: string
+  subscribers_url?: string
+  subscription_url?: string
+  tags_url?: string
+  teams_url?: string
+  trees_url?: string
   clone_url?: string
   mirror_url?: string | null
-  hooks_url: string
+  hooks_url?: string
   svn_url?: string
   homepage?: string | null
   language?: string | null
   forks_count?: number
-  stargazers_count?: number
   watchers_count?: number
   size?: number
   default_branch?: string
@@ -71,7 +72,6 @@ export type Repository = {
   visibility?: string
   pushed_at?: string | null
   created_at?: string | null
-  updated_at?: string | null
   permissions?: {
     admin?: boolean
     maintain?: boolean
@@ -132,34 +132,34 @@ export type Repository = {
 export interface Owner {
   name?: string | null
   email?: string | null
-  login: string
-  id: number
-  node_id: string
-  avatar_url: string
-  gravatar_id: string | null
-  url: string
-  html_url: string
-  followers_url: string
-  following_url: string
-  gists_url: string
-  starred_url: string
-  subscriptions_url: string
-  organizations_url: string
-  repos_url: string
-  events_url: string
-  received_events_url: string
-  type: string
-  site_admin: boolean
+  login?: string
+  id?: number
+  node_id?: string
+  avatar_url?: string
+  gravatar_id?: string | null
+  url?: string
+  html_url?: string
+  followers_url?: string
+  following_url?: string
+  gists_url?: string
+  starred_url?: string
+  subscriptions_url?: string
+  organizations_url?: string
+  repos_url?: string
+  events_url?: string
+  received_events_url?: string
+  type?: string
+  site_admin?: boolean
   starred_at?: string
   user_view_type?: string
   [k: string]: unknown
 }
 
 export interface CodeOfConduct {
-  key: string
-  name: string
-  url: string
+  key?: string
+  name?: string
+  url?: string
   body?: string
-  html_url: string | null
+  html_url?: string | null
   [k: string]: unknown
 }
