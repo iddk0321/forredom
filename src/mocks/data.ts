ac7f880b87f0c61de449c391eb8616db32d688bf
iddk <ldw940321@gmail.com>
2025-03-20 15:39:38 +0900

    feat: 무한스크롤 테스트를 위해 아이템 개수 수정

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,12 +1,12 @@
 import { Repository } from '@/types/github'
 
 export const repositoryMocks: Repository[] = Array.from(
-  { length: 30 },
+  { length: 100 },
   (_, i) => ({
     id: i + 1,
     name: `repo${i + 1}`,
     description: `description${i + 1}`,
-    stargazers_count: i * 10,
+    stargazers_count: 999 - i * 10,
     updated_at: new Date(Date.now() + i * 24 * 60 * 60 * 1000).toISOString(),
   }),
 )
